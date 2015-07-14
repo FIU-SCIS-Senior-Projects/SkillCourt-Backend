63bacc71b571664cf42b4eb1ffaf3734a86fb9d1
Andres Ruggiero <arugg003@fiu.edu>
2015-07-13 21:09:43 -0400

    Parse in index.php and Main.php

--- Code/html/includes/process_login.php
+++ Code/html/includes/process_login.php
@@ -1,20 +1,39 @@
 <?php
-include_once 'db_connect.php';
-include_once 'functions.php';
 
-sec_session_start(); // Our custom secure way of starting a PHP session.
+require 'vendor/autoload.php';
+ 
+use Parse\ParseClient;
+ 
+ParseClient::initialize('pBeFT0fHxcLjMnxwQaiJpb6Ul5HQqayb96X2UHAF', '5ypPgG2h9m7qm78rANzsKa5eQS7MJSZcoLA5OvZr', 'fMAAA9m2Y7CV7OynAePqsqsKXjocgcSHIT0qoVE4');
 
-if (isset($_POST['un'], $_POST['pw'])) {
-    $username = $_POST['un']; // Player's username
-    $password = $_POST['pw']; // The non-hashed password.
-    $userType = $_POST['ut']; // Coach or Player
+use Parse\ParseUser;
+use Parse\ParseObject;
+use Parse\ParseException;
+
+if (isset($_POST['username'], $_POST['password'])) {
+
+    $username = $_POST['username']; // Player's username
+    $password = $_POST['password']; // The non-hashed password.
+    
+    
+    try {
+  		$user = ParseUser::logIn($username, $password);
+  		echo "success";
+  		// Do stuff after successful login.
+	} catch (ParseException $error) {
+  		// The login failed. Check error to see why.
+  		echo $error->getMessage();
+	}
+    
+    /*
     if (login($username, $password, $userType, $mysqli) == true) {
         // Login success
         header('Location: ../Main.php');
     } else {
         // Login failed
         header('Location: ../index.php?error=1');
-    }
+    }*/
+    
 } else {
     // The correct POST variables were not sent to this page.
     echo 'Invalid Request';
--- Code/html/simulator/simulator.pde
+++ Code/html/simulator/simulator.pde
@@ -2257,9 +2257,15 @@ class FlyRoutine extends Routine
       ArrayList rowHit = (myRoom.getWallID(x, y) == wall1)  ? row1 : row2 ;
       setRowToColor(rowHit, padOffColor) ;
       while( rowHit.size() > 0 ) rowHit.remove(0) ;   
-      generateStep() ;  
+      generateStep() ; 
+      return true; 
     }
-    else if (myRoom.colorOfClick(x, y) == padOffColor) myStats.miss() ;
+    else if (myRoom.colorOfClick(x, y) == padOffColor) {
+      myStats.miss() ;
+      println("You missed");
+    } else if (myRoom.colorOfClick(x, y) == red) {
+      myStats.minusPoint();
+    }
     
     return false;
   }   
@@ -2350,8 +2356,13 @@ class ChaseRoutine extends Routine
       setRowToColor(rowHit, padOffColor) ;
       while( rowHit.size() > 0 ) rowHit.remove(0) ;   
       generateStep() ;  
+      return true;
     }
-    else if(myRoom.colorOfClick(x, y) == padOffColor) myStats.miss() ;
+    else if(myRoom.colorOfClick(x, y) == padOffColor) {
+      myStats.miss() ;
+    } else if (myRoom.colorOfClick(x, y) == red) {
+      myStats.minusPoint();
+    }
     
     return false;
   }  
