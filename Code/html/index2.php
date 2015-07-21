5978d243d7ac6276f8d3da1a0b6467a3940cdf27
eoyarzun <eoyarzun6668@gmail.com>
2015-07-21 13:13:11 -0400

    For test site

--- Code/html/index2.php
+++ Code/html/index2.php
@@ -1,68 +1,81 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
+    
+    include_once("parseHeader.php");
+    
+    use Parse\ParseObject;
+    use Parse\ParseException;
+    
+    $errorMessage  = "" ;
+    
+    if (isset($_GET["error"])) {
+        $errorMessage = "Invalid Login Credentials";
+        echo $errorMessage;
+    }
 ?>
 
-<?php
-include_once 'includes/db_connect.php';
-include_once 'includes/functions.php';
- 
-sec_session_start();
- 
-if (login_check($mysqli) == true) {
-    $logged = 'in';
-} else {
-    $logged = 'out';
-}
-?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
     <head>
         <title>Secure Login: Log In</title>
-        <link rel="stylesheet" href="style/Main.css" />
-        <script type="text/JavaScript" src="js/sha512.js"></script>
-        <script type="text/JavaScript" src="js/forms.js"></script>
+        <link rel="stylesheet" href="style/index.css" />
+        <meta charset="UTF-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="//www.parsecdn.com/js/parse-1.5.0.min.js"></script>
+        <script src="parse_php/parse.js"></script>
+        <script src="home_jquery.js"></script>
     </head>
-    <body>
-        <div id="MainData">
-	    <div id="Header">
-		SkillCourt
-	    </div>
-
-	    <div id="User">
-	    </div>
-	<div id="Menu">
-                <h2><a href="Main2.php">Home</a></h2>
-                <h2><a href="Statistics2.php">Statistics</a></h2>
-                <h2><a href="routines2.php">Routines</a></h2>
-	</div>
-	<div id="Content">
-            <?php
-            if (isset($_GET['error'])) {
-                echo '<p class="error">Error Logging In!</p>';
-            }
-            ?>
- 	    <form action="includes/process_login.php" method="post" name="login_form">
-                Username: <input type="text" name="un" /><br />
-                Password: <input type="password"
-                                 name="pw"
-                                 id="password"/><br />
-                <input type="button"
-                       value="Login"
-                       onclick="formhash(this.form, this.form.password);" />
+<body>
+    <div><img src="style/images/soccer2.jpg" id="bg" alt=""></div>
+    <div id="navigationBar">
+        <div class="navigationButtons" id="buttonGroup">
+            <button id="homeButton" onclick="location.href = 'index.php';">Home</button>
+            <button id="aboutButton">About</button>
+			<button id="routineButton" onclick="location.href = 'customWizard.php';">Wizard</button>
+			<button id="simulatorButton" onclick="location.href = 'simulator.php';">Simulator</button>
+        </div>
+        <div id="text">SkillCourt</div>
+    </div>
+    <div id="registerNowZoom">
+        <div id="zoomRectangle">
+            <div id="zoomOval"></div>
+            <button onclick="location.href = 'create.php';" id="registerNowButton" disabled>REGISTER NOW</button>
+            <div id="registerNowHeader">and start playing!</div>
+        </div>
+    </div>
+    <button id="logInButton" href = "javascript:void(0)" >Log In</button>
+    <div id="light" class="white_content">This is the lightbox content.
+        <div id="logInRectangle">
+            <div id="headerLogInRectangle">Log In or Create an Account
+                <button id="exitButton"> &#10006; </button>
+            </div>
+            <div>
+                <form action="process_login.php" method="POST" name="login_form">
+                    <input type="text" name="username" id="username" placeholder="Username" required ><br>
+                    <input type="password" name="password" id="password" placeholder="Password" required ><br>
+                    <input type="submit" id="popUplogInButton" value ="LOG IN" disabled><br>
+                </form>
+                <button id="changePasswordButton" onclick="change_password();">Forgot your username or password?</button>
+                <div id="signUpHeader">Don&apos;t have an account?</div>
+                <a id="signupButton" href=# >Sign Up</a>
+            </div>
+        </div>
+        <div id="changePasswordRectangle">
+            <div id="headerChangePasswordRectangle">Password Reset
+                <button id="exitChangePasswordButton" onclick="resetLogInWindow();"> &#10006; </button>
+            </div>
+            <div id="passwordResetInformation">Enter your SkillCourt email address that you used to register. We&apos;ll send you an email with your username and a link to reset your password.</div>
+            <form action="change_password.php" method="POST" name="change_password_form" disabled>
+                <input type="text" name="emailAddressForPasswordChange" id="emailAddressForPasswordChange" placeholder="Email Address" required disabled><br>
+                <input type="submit" id="submitPasswordChange" value="SEND" disabled><br>
             </form>
-	    <?php
-            if (login_check($mysqli) == true) {
-                echo '<p>Currently logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.</p>';
-                echo '<p>Do you want to change user? <a href="includes/logout.php">Log out</a>.</p>';
-            } else {
-                echo '<p>Currently logged ' . $logged . '.</p>';
-                echo "<p>If you don't have a login, please <a href='register2.php'>register</a></p>";
-            }
-	    ?>
- 	    </div>
-
-	<div id="Footer">Copyright 2015 Skillcourt</div>
-    </body>
+        </div>
+    </div>
+    <div id="fade" class="black_overlay"></div>
+    <div id="rectangle2"> </div>
+    <div id="informationRectangle"></div>
+    <div id="textRectangle">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+    <div id="pageFooter"></div>
+</body>
 </html>
