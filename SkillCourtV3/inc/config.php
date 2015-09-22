1d3ac3cbff4cd410881ae52ab913654f0dcdea5e
Will Rodriguez <wrodr032@fiu.edu>
2015-09-21 22:57:35 -0400

    Delete config.php

--- SkillCourtV3/inc/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // these two constants are used to create root-relative web addresses
-    // and absolute server paths throughout all the code
-	define("BASE_URL","/");
-	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/");
-	define('ROOT', __DIR__);
-?>
