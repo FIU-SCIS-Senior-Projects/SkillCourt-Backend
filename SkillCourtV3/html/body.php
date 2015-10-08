b250ac290741dd86865046c0a9488c46f2743604
wrodr032 <wrodr032@fiu.edu>
2015-10-08 11:29:04 -0400

    Added about page to the index page. Added logic to be able to access this page when user has not logged in. And once the user has logged in.

--- SkillCourtV3/html/body.php
+++ SkillCourtV3/html/body.php
@@ -1,40 +1,52 @@
 <?php
 if($userNotLogged)
 {
-	echo "
-	<div class=\"container contBorders\">
-		<div class=\"row\">
-			<div class=\"col-md-12 text-center\" >
-				
-				<h2 class=\"modifyText\">Place holder for FRAME!!</h2>
-				<!-- Frame goes here -->
-				<img class=\"img-responsive center-block\" src=\"./img/stadium.jpg\">
+	if(isset($_GET['show']))
+	{
+		if($_GET['show'] == 'about')
+		{
+			//Show the about page
+			include_once './view/about.php';
+		}
+	}
+	else
+	{
+		//Show the index page. This page will be shown on load up. 
+		echo "
+		<div class=\"container contBorders\">
+			<div class=\"row\">
+				<div class=\"col-md-12 text-center\" >
+					
+					<h2 class=\"modifyText\">Place holder for FRAME!!</h2>
+					<!-- Frame goes here -->
+					<img class=\"img-responsive center-block\" src=\"./img/stadium.jpg\">
 
+				</div>
 			</div>
 		</div>
-	</div>
 
-	<br/>
+		<br/>
 
-	<div class=\"container contBorders modifyText\">
-		<div class=\"row\">
-			<div class=\"col-md-12 text-center\">
-				<h1>USER MODES</h1>
+		<div class=\"container contBorders modifyText\">
+			<div class=\"row\">
+				<div class=\"col-md-12 text-center\">
+					<h1>USER MODES</h1>
+				</div>
+			</div>
+
+			<div class=\"row\">
+				<div class=\"col-md-6\">
+					<h1 class=\"text-center\">Coach Mode</h1>
+					<!-- Description of Coach Mode -->
+				</div>
+				<div class=\"col-md-6\">
+					<h1 class=\"text-center\">Player Mode</h1>
+					<!-- Description of Player Mode -->
+				</div>
 			</div>
 		</div>
-
-		<div class=\"row\">
-			<div class=\"col-md-6\">
-				<h1 class=\"text-center\">Coach Mode</h1>
-				<!-- Description of Coach Mode -->
-			</div>
-			<div class=\"col-md-6\">
-				<h1 class=\"text-center\">Player Mode</h1>
-				<!-- Description of Player Mode -->
-			</div>
-		</div>
-	</div>
-	";
+		";
+	}
 }
 else
 {
