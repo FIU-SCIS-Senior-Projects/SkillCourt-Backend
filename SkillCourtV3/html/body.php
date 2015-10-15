452838c897b18efeec92be5d3fbc540d05529577
Sergio Saucedo <ssauc002@fiu.edu>
2015-10-15 15:12:37 -0400

    Added showcase video and interactive summary table to body.php

--- SkillCourtV3/html/body.php
+++ SkillCourtV3/html/body.php
@@ -1,52 +1,167 @@
 <?php
 if($userNotLogged)
 {
-	if(isset($_GET['show']))
-	{
-		if($_GET['show'] == 'about')
-		{
-			//Show the about page
-			include_once './view/about.php';
-		}
-	}
-	else
-	{
-		//Show the index page. This page will be shown on load up. 
-		echo "
-		<div class=\"container contBorders\">
-			<div class=\"row\">
-				<div class=\"col-md-12 text-center\" >
-					
-					<h2 class=\"modifyText\">Place holder for FRAME!!</h2>
-					<!-- Frame goes here -->
-					<img class=\"img-responsive center-block\" src=\"./img/stadium.jpg\">
+	echo " 
+    <div class=\"container contBorders\">
+        <div class=\"row\">
+            <div class=\"col-md-12 text-center\" >
 
-				</div>
-			</div>
-		</div>
+                </br>
+                <!-- Video Frame -->
+                <video class=\"videoBorder\" width=\"1120\" height=\"630\" autoplay controls loop>
+                    <source src=\"./videos/SkillCourtShowcase.mp4\" type=\"video/mp4\">
+                    Upgrade you browser to see video!.
+                </video>
 
-		<br/>
+                <!--img class=\"img-responsive center-block\" src=\"./img/stadium.jpg\"-->
+            </div>
+        </div>
+    </div>
 
-		<div class=\"container contBorders modifyText\">
-			<div class=\"row\">
-				<div class=\"col-md-12 text-center\">
-					<h1>USER MODES</h1>
-				</div>
-			</div>
+    <br/>
 
-			<div class=\"row\">
-				<div class=\"col-md-6\">
-					<h1 class=\"text-center\">Coach Mode</h1>
-					<!-- Description of Coach Mode -->
-				</div>
-				<div class=\"col-md-6\">
-					<h1 class=\"text-center\">Player Mode</h1>
-					<!-- Description of Player Mode -->
-				</div>
-			</div>
-		</div>
-		";
-	}
+    <div class=\"container contBorders modifyText\">
+        <div class=\"row\">
+            <div class=\"col-md-12 text-center\">
+                <!-- Interactive Table -->
+
+                 <div style=\"width:1009px; height:460px; margin-left:60px; margin-top: 12px; padding-top:15px;
+                 font-family:Arial, Helvetica, sans-serif; color:#FFF; text-align:left; background-color:#F4F4F4;
+                 -moz-border-radius: 8px; border-radius: 8px; border-color:#CCC; border-style:solid; border-width:thin;\">
+
+                    <div id=\"nav\" style=\"width:260px; height:425px; float:left; margin-top:10px; 
+                    border-right-color:#CCC; border-right-style:solid; border-right-width:thin; padding-right:22px; 
+                    margin-left:5px; padding-left:15px; font-weight:bold; font-size:14px;\">
+
+                        <a id=\"goto1\" style=\"cursor:pointer;\">
+                            <div style=\"width:229px; height:64px;\" onmouseover=\"this.style.opacity=0.8;this.filters.alpha.opacity=80\" 
+                            onmouseout=\"this.style.opacity=1;this.filters.alpha.opacity=100\">
+                    
+                                <div style=\"position:absolute; z-index:99; margin-top:20px; margin-left:15px; color:white;\">SkillCourt</div>
+                                <img src=\"img/BgrndTabbedTitles.png\" width=\"229\" height=\"64\" border=\"0\" />
+                            </div>          
+                        </a>
+
+                        <div style=\"position:absolute; z-index:99; margin-top:10px; margin-left:15px; color:white;\">User Modes</div>
+                        <img src=\"img/MainBgrndTabbedTitles.png\" width=\"229\" height=\"35\" border=\"0\" style=\"margin-top:2px; margin-bottom:-2px;\" />
+                   
+                        <a id=\"goto2\" style=\"cursor:pointer;\">
+                            <div style=\"margin-top:5px;\" onmouseover=\"this.style.opacity=0.8;this.filters.alpha.opacity=80\" 
+                            onmouseout=\"this.style.opacity=1;this.filters.alpha.opacity=100\">
+                
+                                <div style=\"position:absolute; z-index:99; margin-top:20px; margin-left:15px; color:white;\">Player</div>
+                                <img src=\"img/BgrndTabbedTitles.png\" width=\"229\" height=\"64\" border=\"0\" />
+                            </div>
+                        </a>
+
+                        <a id=\"goto3\" style=\"cursor:pointer;\">
+                            <div style=\"margin-top:5px;\" onmouseover=\"this.style.opacity=0.8;this.filters.alpha.opacity=80\" 
+                            onmouseout=\"this.style.opacity=1;this.filters.alpha.opacity=100\">
+                
+                                <div style=\"position:absolute; z-index:99; margin-top:20px; margin-left:15px; color:white;\">Coach</div>
+                                <img src=\"img/BgrndTabbedTitles.png\" width=\"229\" height=\"64\" border=\"0\" />
+                            </div>
+                        </a>
+
+                        <div style=\"position:absolute; z-index:99; margin-top:10px; margin-left:15px; color:white;\">Components</div>
+                        <img src=\"img/MainBgrndTabbedTitles.png\" width=\"229\" height=\"35\" border=\"0\" style=\"margin-top:2px; margin-bottom:-2px;\" />
+
+                        <a id=\"goto4\" style=\"cursor:pointer;\">
+                            <div style=\"margin-top:5px;\" onmouseover=\"this.style.opacity=0.8;this.filters.alpha.opacity=80\" 
+                            onmouseout=\"this.style.opacity=1;this.filters.alpha.opacity=100\">
+                                
+                                <div style=\"position:absolute; z-index:99; margin-top:20px; margin-left:15px; color:white;\">Simulator</div>
+                                <img src=\"img/BgrndTabbedTitles.png\" width=\"229\" height=\"64\" border=\"0\" />
+                            </div>
+                        </a>
+
+                        <a id=\"goto5\" style=\"cursor:pointer;\">
+                            <div style=\"margin-top:5px;\" onmouseover=\"this.style.opacity=0.8;this.filters.alpha.opacity=80\" 
+                            onmouseout=\"this.style.opacity=1;this.filters.alpha.opacity=100\">
+                                
+                                <div style=\"position:absolute; z-index:99; margin-top:20px; margin-left:15px; color:white;\">Routine Wizard</div>
+                                <img src=\"img/BgrndTabbedTitles.png\" width=\"229\" height=\"64\" border=\"0\" />
+                            </div>
+                        </a>
+                    </div>
+  
+                    <div id=\"s1\" style=\"width:700px; height:220px; float:left; margin-top:10px; margin-left:22px; text-align:left;\">
+                        
+                        <!-- SkillCourt Tab View -->
+                        <div style=\"background-color:#F4F4F4; height:150px;\">
+                        
+                            <div style=\"font-size:21px; color:#2f7b2a;\">
+                                 The objective is to produce a modern system for training soccer players.
+                            </div>
+                        
+                            <div style=\"font-size:13px; color:#666; margin-top:5px; line-height:20px;\">
+                                There is a lot involved with the training of soccer players. The current system for training 
+                                is primitive usually involving an instructor and a physical field for playing. The system will 
+                                be a program with features that will assist players for learning the skills required on their 
+                                own.<br /><br />
+                            
+                                <div style=\"float:right; width:300px; height:150px; margin-top:20px; margin-left:-600px; margin-right:44px;\">
+                                    Implementing this system is revolutionary to the way avid players train in the sport.
+                                </div>
+
+                                <div>
+                            
+                                    <div style=\"float:left; width:336px; height:285px;\">
+                                    <img src=\"img/room.png\" width=\"336\" height=\"285\" border=\"0\" />
+                                    </div>
+                                
+                                    <div style=\"float:left; width:300px; height:300px; margin-top:100px; margin-left:20px;\">
+                                        With the functionality and portability that SkillCourt offers, the user can create a 
+                                        personalized regimen for improving skills. Thus, SkillCourt offers an overall improvement 
+                                        to both the soccer training and playing experience for players.
+                                    </div>
+                            
+                                </div> 
+                            </div>
+                        </div>
+                        <!-- -->
+
+                        <!-- Player Tab View -->
+                        <div style=\"display:none; background-color:#F4F4F4; height:150px;\">
+                        
+                            <div style=\"font-size:21px; color:#2f7b2a;\">
+                                Information about player.
+                            </div>                         
+                        </div>
+                        <!-- -->
+
+                        <!-- Coach Tab View -->
+                        <div style=\"display:none; background-color:#F4F4F4; height:150px;\">
+                        
+                            <div style=\"font-size:21px; color:#2f7b2a;\">
+                                Information about coach.
+                            </div>                       
+                        </div>
+                        <!-- -->
+
+                        <!-- Simulator Tab View -->
+                        <div style=\"display:none; background-color:#F4F4F4; height:150px;\">
+                        
+                            <div style=\"font-size:21px; color:#2f7b2a;\">
+                                Information about simulator.
+                            </div>
+                        </div>
+                        <!-- -->
+
+                         <!-- Routine Wizard Tab View -->
+                        <div style=\"display:none; background-color:#F4F4F4; height:150px;\">
+                        
+                            <div style=\"font-size:21px; color:#2f7b2a;\">
+                                Information about routine wizard.
+                            </div>
+                        </div>
+                        <!-- -->
+                    
+                    </div>         
+                </div>
+            </div>
+        </div>
+    ";
 }
 else
 {
@@ -57,7 +172,7 @@ else
         switch ($page) {
             case 'home':
                 //Index page
-            	include_once './view/home.php';
+
                 break;
             case 'about':
                  //about.php
