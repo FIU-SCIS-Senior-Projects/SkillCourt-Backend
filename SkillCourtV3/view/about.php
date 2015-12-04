b0edfcb2cc9a9a22c52b366754d83c05bf4affe6
Sergio Saucedo <ssauc002@fiu.edu>
2015-12-03 20:58:44 -0500

    Updated about.php. Now is clearer are easier to read

--- SkillCourtV3/view/about.php
+++ SkillCourtV3/view/about.php
@@ -1,7 +1,6 @@
-<div class="container contBorders" style>
+<div class="container leContainer" style>
     <div class="row\">
         <div class="col-md-12 text-left" >
-
             <div id="introductionChallengeSolution">
                 <div id="introductionSection" class="mainText">
                     <h2>Introduction</h2>
@@ -18,56 +17,52 @@
                         a multiple color illuminations to guide the user for a particular game, 
                         the more pads the more targets user can play with.</p>
                 </div>
-
-                <div id="sectionSeparator" class="mainText" style="font-size:medium;">
-                    <p>..............................................................................................................................................................................................</p>
-                </div>
-
-                <div id="challengeSection" class="mainText" style="float:left; width:525px;">
-                    <h2>Challenge</h2>
-                    <p id="challengeParagraph">
-                        Soccer players at all ages lack the exposure to efficient commercialized Soccer 
-                        technology. Every player who wants to improve would for example love to receive 
-                        any measurable statistics about their individual performance. Also, there hasn’t 
-                        been a lot of breakthrough technological advancements in products for Soccer 
-                        players to use when practicing individually outside of practice hours. Combine 
-                        that with the ongoing neuroscientific research by leading experts in sports which 
-                        emphasizes the importance on the athlete’s brain power (i.e. cognitive skills). 
-                        Neuroscience suggests that brain powering activities plays a huge role when 
-                        players prepare physically &amp; mentally for those key moments during games to 
-                        execute a move at the right time with the desired motor skill. All Soccer 
-                        enthusiasts know that with each move on the field could have a domino effect on 
-                        good things or bad to come.</p>
-                </div>
-
-                <div id="sectionSeparator" class="mainText" style="position: absolute; left:410px; top:450px; font-size:medium; -webkit-transform: rotate(90deg);
-                        -moz-transform: rotate(90deg);-o-transform: rotate(90deg);-ms-transform: rotate(90deg);
-                        transform: rotate(90deg);">
-                    <p>....................................................</p>
-                </div>
-
-                <div id="solutionSection" class="mainText" style="float:right; width:525px;">
-                    <h2>Solution</h2>
-                    <p id="challengeParagraph">
-                        Soccer players at all ages lack the exposure to efficient commercialized soccer 
-                        technology. Every player who wants to improve would for example love to receive 
-                        any measurable statistics about their individual performance. Also, there hasn’t 
-                        been a lot of breakthrough technological advancements in products for Soccer 
-                        players to use when practicing individually outside of practice hours. Combine 
-                        that with the ongoing neuroscientific research by leading experts in sports which 
-                        emphasizes the importance on the athlete’s brain power (i.e. cognitive skills). 
-                        Neuroscience suggests that brain powering activities plays a huge role when 
-                        players prepare physically &amp; mentally for those key moments during games to 
-                        execute a move at the right time with the desired motor skill. All Soccer 
-                        enthusiasts know that with each move on the field could have a domino effect on 
-                        good things or bad to come.</p>
-                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row\">
+        <div class="col-md-12 text-left" >
+            <div id="challengeSection" class="mainText">
+                <h2>Challenge</h2>
+                <p id="challengeParagraph">
+                    Soccer players at all ages lack the exposure to efficient commercialized Soccer 
+                    technology. Every player who wants to improve would for example love to receive 
+                    any measurable statistics about their individual performance. Also, there hasn’t 
+                    been a lot of breakthrough technological advancements in products for Soccer 
+                    players to use when practicing individually outside of practice hours. Combine 
+                    that with the ongoing neuroscientific research by leading experts in sports which 
+                    emphasizes the importance on the athlete’s brain power (i.e. cognitive skills). 
+                    Neuroscience suggests that brain powering activities plays a huge role when 
+                    players prepare physically &amp; mentally for those key moments during games to 
+                    execute a move at the right time with the desired motor skill. All Soccer 
+                    enthusiasts know that with each move on the field could have a domino effect on 
+                    good things or bad to come.</p>
+            </div>
+        </div>
+    </div>
+    <div class="row\">
+        <div class="col-md-12 text-left" >
+            <div id="solutionSection" class="mainText">
+                <h2>Solution</h2>
+                <p id="challengeParagraph">
+                    Soccer players at all ages lack the exposure to efficient commercialized soccer 
+                    technology. Every player who wants to improve would for example love to receive 
+                    any measurable statistics about their individual performance. Also, there hasn’t 
+                    been a lot of breakthrough technological advancements in products for Soccer 
+                    players to use when practicing individually outside of practice hours. Combine 
+                    that with the ongoing neuroscientific research by leading experts in sports which 
+                    emphasizes the importance on the athlete’s brain power (i.e. cognitive skills). 
+                    Neuroscience suggests that brain powering activities plays a huge role when 
+                    players prepare physically &amp; mentally for those key moments during games to 
+                    execute a move at the right time with the desired motor skill. All Soccer 
+                    enthusiasts know that with each move on the field could have a domino effect on 
+                    good things or bad to come.</p>
             </div>
         </div>
     </div>
 </div>
 
-<div class="container contBorders">
+<div class="container leContainer">
     <div class="row\">
         <div class="col-md-12 text-left" >
             
